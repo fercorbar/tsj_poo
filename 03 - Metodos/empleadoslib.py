8e32129cec6b7346bb043c58c6d5c720418146b1
Fernando Córdova <fernando@domus.mx>
2024-02-22 19:26:30 -0600

    correcciones

--- 03 - Metodos/empleadoslib.py
+++ 03 - Metodos/empleadoslib.py
@@ -7,6 +7,9 @@ class Empleado:
         self.telefono = telefono
         self.genero = genero
 
+    def info(self):
+        return f"{self.nombre}, edad: {self.edad}, gen: {self.genero}, tel: {self.telefono}"
+
 class EmpleadoTemporal(Empleado):
     def __init__(self, nombre, edad, telefono, genero, fecha_termino):
         self.fecha_termino = fecha_termino
@@ -18,28 +21,49 @@ class EmpleadoIndefinido(Empleado):
         super(EmpleadoIndefinido, self).__init__(nombre, edad, telefono, genero)
     
     def calcular_aumento(self, porcentaje):
-        return math.round(self.salario * (porcentaje/100),2)
+        return self.salario * (porcentaje/100)
 
 class Empresa:
     def __init__(self, nombre):
         self.nombre = nombre
         self.__empleados = []
 
-    def contratar(self, empleado):
-        empleado_ya_esta_registrado = False
-        for empleado_registrado in self.__empleados:
-            if empleado_registrado.nombre == empleado.nombre:
-                empleado_ya_esta_registrado = True
-        if not empleado_ya_esta_registrado:
-            self.__empleados.append(empleado)
-            # print(f"{empleado.nombre}, bienvenido a tu empresa {self.nombre}")
-        # else:
-        #     print(f"El empleado {empleado.nombre} no se puede contratar ya se encuentra contratado")
+    # def __contratar_uno(self, empleado):
+    #     empleado_ya_esta_registrado = False
+    #     for empleado_registrado in self.__empleados:
+    #         if empleado_registrado.nombre == empleado.nombre:
+    #             empleado_ya_esta_registrado = True
+    #     if not empleado_ya_esta_registrado:
+    #         self.__empleados.append(empleado)
+
+
+    # def contratar(self, personas_a_contratar, multiple = False):
+    #     if multiple:
+    #         for persona in personas_a_contratar:
+    #             self.__contratar_uno(persona)
+    #     else:
+    #         self.__contratar_uno(personas_a_contratar)
+
+
+    def contratar(self, personas_a_contratar, multiple = False):
+        if multiple:
+            aux = personas_a_contratar
+        else:
+            aux = [personas_a_contratar]
+        for persona in aux:
+            empleado_ya_esta_registrado = False
+            for empleado_registrado in self.__empleados:
+                if empleado_registrado.nombre == persona.nombre:
+                    empleado_ya_esta_registrado = True
+            if not empleado_ya_esta_registrado:
+                self.__empleados.append(persona)
+
+
 
     def despedir(self, nombre_empleado):
         for empleado in self.__empleados:
-            if empleado == nombre_empleado:
-                self.__empleados.pop(empleado)
+            if empleado.nombre == nombre_empleado:
+                self.__empleados.remove(empleado)
                 break
 
     def visualizar_empleados(self, compacto = True):
@@ -56,8 +80,12 @@ class Empresa:
                 print("No hay empleados")
             else:
                 for empleado in self.__empleados:
-                    print(f"{empleado.nombre} Edad: {empleado.edad}, Género {empleado.genero}, Tel: {empleado.telefono}")
+                    print(empleado.info())
 
-    
+    def buscar_empleado(self, texto_busqueda):
+        for empleado in self.__empleados:
+            if texto_busqueda in empleado.nombre:
+                print(empleado.info())
+                break
 
     
